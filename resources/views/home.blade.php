166f975433a1ab22df5bc5aa78bc45ec3f05cd7b
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-05 08:50:42 +0200

    use model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-        <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
-        </div>
 
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel model controller</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        <h1>Movies List</h1>
+
+        <div class="row mt-3">
+            @foreach ($movies as $movie)
+            <div class="col-4 gap-3 mb-3">
+                <div class="card">
+                    <h2>{{ $movie->title }}</h2>
+                    <h3>{{ $movie->original_title }}</h3>
+                    <p>{{ $movie->nationality }}</p>
+                    <p> {{ $movie->date }}</p>
+                    <p>{{ $movie->vote }}</p>
+                </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+</body>
+
 </html>
